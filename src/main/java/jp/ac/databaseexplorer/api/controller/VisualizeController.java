8b4fc521005ed450d416faa10a0302ca7d180b98
r-horikiri <r-horikiri@medi-system.co.jp>
2023-05-14 00:06:53 +0900

    remove: 要らなくなったクラスや記述

--- src/main/java/jp/ac/databaseexplorer/api/controller/VisualizeController.java
+++ src/main/java/jp/ac/databaseexplorer/api/controller/VisualizeController.java
@@ -78,10 +78,4 @@ public class VisualizeController {
   public ResponseEntity<CpuUsageApiResponse> cpuResource() {
     return new ResponseEntity<>(HttpStatus.OK);
   }
-
-  @GetMapping
-  @RequestMapping(value = "/csvtest")
-  public void cpuCsvTest() {
-
-  }
 }
