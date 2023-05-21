902757a28a480ac92dbeac67e74c709d03160627
Kyoko Yoshida <a2244ky@aiit.ac.jp>
2023-05-21 18:14:11 +0900

    feat: Postgresのプロセスのサービスを実装

---
+++ src/main/java/jp/ac/databaseexplorer/api/model/visualization/PostgresProcessData.java
@@ -0,0 +1,48 @@
+package jp.ac.databaseexplorer.api.model.visualization;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.util.Date;
+
+/**
+ * PosgreSQLのプロセスの状態を保持するモデルクラス
+ */
+@Data
+@AllArgsConstructor
+public class PostgresProcessData {
+
+  @NonNull
+  @JsonProperty("date") //なぜ必要？
+  @JsonFormat(pattern = "yyyy/MM/dd HH:mm:ss", timezone = "Asia/Tokyo")
+  private Date timestamp;
+
+  @JsonProperty("masterProcess")
+  private Boolean masterProcess;
+
+  @JsonProperty("checkPointer")
+  private Boolean checkPointer;
+
+  @JsonProperty("autoVacuumLauncher")
+  private Boolean autoVacuumLauncher;
+
+  @JsonProperty("walWriter")
+  private Boolean walWriter;
+
+  @JsonProperty("writer")
+  private Boolean writer;
+
+  @JsonProperty("statisticsCollector")
+  private Boolean statisticsCollector;
+
+  @JsonProperty("autoVacuumWorker")
+  private Boolean autoVacuumWorker;
+
+  @JsonProperty("backendProcess")
+  private Boolean backendProcess;
+
+
+}
--- src/main/java/jp/ac/databaseexplorer/api/service/visualization/PostgresProcessService.java
+++ src/main/java/jp/ac/databaseexplorer/api/service/visualization/PostgresProcessService.java
@@ -2,8 +2,13 @@ package jp.ac.databaseexplorer.api.service.visualization;
 
 import jp.ac.databaseexplorer.api.model.visualization.PostgresProcessApiRequest;
 import jp.ac.databaseexplorer.api.model.visualization.PostgresProcessApiResponse;
+import jp.ac.databaseexplorer.common.component.csv.impl.ProcessStatusCsvReader;
+import jp.ac.databaseexplorer.storage.visualization.ProcessStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * Postgresのプロセス情報を取得するサービスクラス
@@ -13,9 +18,31 @@ import org.springframework.stereotype.Service;
 public class PostgresProcessService {
 
   /**
+   * Reader
+   */
+  private final ProcessStatusCsvReader reader;
+
+  /**
    * Postgresのプロセス情報を取得する
    */
   public PostgresProcessApiResponse getPostgresProcess(PostgresProcessApiRequest request) {
-    return null;
+    try {
+      Date startTime = request.getStartTime();
+      Date endTime = request.getEndTime();
+      List<ProcessStatus> postgresProcessData = reader.read(startTime, endTime);
+
+      boolean masterProcess = !(postgresProcessData.stream().anyMatch(processStatus -> !processStatus.getMasterProcess()));
+      boolean backendProcess = !(postgresProcessData.stream().anyMatch(processStatus -> !processStatus.getBackendProcess()));
+      boolean autoVacuumLauncher = !(postgresProcessData.stream().anyMatch(processStatus -> !processStatus.getAutoVacuumLauncher()));
+      boolean checkPointer = !(postgresProcessData.stream().anyMatch(processStatus -> !processStatus.getCheckPointer()));
+      boolean statisticsCollector = !(postgresProcessData.stream().anyMatch(processStatus -> !processStatus.getStatisticsCollector()));
+      boolean walWriter = !(postgresProcessData.stream().anyMatch(processStatus -> !processStatus.getWalWriter()));
+      boolean writer = !(postgresProcessData.stream().anyMatch(processStatus -> !processStatus.getWriter()));
+      boolean autoVacuumWorker = !(postgresProcessData.stream().anyMatch(processStatus -> !processStatus.getAutoVacuumWorker()));
+
+      return new PostgresProcessApiResponse(startTime, endTime, masterProcess, walWriter, writer, checkPointer, statisticsCollector, autoVacuumLauncher, autoVacuumWorker, backendProcess);
+    } catch (Exception e) {
+      return null;  // TODO: 例外処理
+    }
   }
 }
