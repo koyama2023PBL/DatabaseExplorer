8d184ef24e49e4c0ba62520386396080db1d0f64
Kyoko Yoshida <a2244ky@aiit.ac.jp>
2023-05-22 22:06:55 +0900

    refactor: スロークエリ数取得サービスのJavaDocを修正

--- src/main/java/jp/ac/databaseexplorer/storage/visualization/QueryTime.java
+++ src/main/java/jp/ac/databaseexplorer/storage/visualization/QueryTime.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 /**
- * ＤＢより取得したクエリの実行時間（プロセスごと）を保持するモデルクラス
+ * DBより取得したクエリの実行時間（プロセスごと）を保持するモデルクラス
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
