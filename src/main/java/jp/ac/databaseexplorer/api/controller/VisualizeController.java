61cb51e2f631eaf9d7ff5b2af1ac59c73f2fde95
Feng Yiwei <a2235yf@aiit.ac.jp>
2023-05-24 22:56:54 +0900

    feat:コントロールクラス（可視化層のコントローラに実行時間とログを追加）

--- src/main/java/jp/ac/databaseexplorer/api/controller/VisualizeController.java
+++ src/main/java/jp/ac/databaseexplorer/api/controller/VisualizeController.java
@@ -68,101 +68,35 @@ public class VisualizeController {
   @RequestMapping(value = "/cpu-usage", method = RequestMethod.GET)
   public ResponseEntity<?> cpuUsage( @RequestParam("starttime")String startTime,@RequestParam("endtime")String endTime) {
     try {
+      long methodBeginTime = System.currentTimeMillis();
+      systemLogger.info("cpuUsage start。 startTime:" + startTime + " endTime:" + endTime);
+
       SimpleDateFormat dtFt = new SimpleDateFormat(STRING_TO_DATE);
       Date startTimeDate = dtFt.parse(startTime);
       Date endTimeDate = dtFt.parse(endTime);
       CpuUsageApiRequest request = new CpuUsageApiRequest(startTimeDate, endTimeDate);
       CpuUsageApiResponse response = cpuUsageService.getCpuUsage(request);
 
-      // ダミーのレスポンスを作成する --START
-      // サービスの実装が完了次第、作成する予定
-      SimpleDateFormat dummyDtFt = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-      CpuUsageData[] cpuUsageDatas = new CpuUsageData[]{
-        new CpuUsageData(dummyDtFt.parse("2023/05/07 18:00:00"), 20.98),
-        new CpuUsageData(dummyDtFt.parse("2023/05/07 18:10:00"), 30.98),
-        new CpuUsageData(dummyDtFt.parse("2023/05/07 18:20:00"), 40.98),
-        new CpuUsageData(dummyDtFt.parse("2023/05/07 18:30:00"), 20.88),
-        new CpuUsageData(dummyDtFt.parse("2023/05/07 18:40:00"), 20.68),
-        new CpuUsageData(dummyDtFt.parse("2023/05/07 18:50:00"), 25.98),
-        new CpuUsageData(dummyDtFt.parse("2023/05/07 19:00:00"), 20.98),
-        new CpuUsageData(dummyDtFt.parse("2023/05/07 19:10:00"), 20.98),
-        new CpuUsageData(dummyDtFt.parse("2023/05/07 19:20:00"), 30.98),
-        new CpuUsageData(dummyDtFt.parse("2023/05/07 19:30:00"), 20.98),
-        new CpuUsageData(dummyDtFt.parse("2023/05/07 19:40:00"), 70.68),
-        new CpuUsageData(dummyDtFt.parse("2023/05/07 19:50:00"), 10.28),
-        new CpuUsageData(dummyDtFt.parse("2023/05/07 20:00:00"), 20.98)
-      };
-      response = new CpuUsageApiResponse(startTimeDate, endTimeDate, cpuUsageDatas);
-      // ダミーのレスポンスを作成する --END
+      systemLogger.info("cpuUsage end。 Response:" + response);
+      long methodEndTime = System.currentTimeMillis();
+      operationLogger.info("cpuUsage operationTime:" + (methodEndTime - methodBeginTime) + " ms");
 
       return ResponseEntity.ok(response);
       }
-    catch (ParseException e) {
+    catch (ParseException pe) {
+      errorLogger.error("cpuUsage:ParseException", pe);
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+    catch (ApplicationException ae) {
+      errorLogger.error("cpuUsage:ApplicationException", ae);
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
     catch (Exception e) {
+      errorLogger.error("cpuUsage:Exception", e);
       return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
-  /**
-   * CPU使用率を取得するAPI
-   * @param startTime 取得期間の開始時間
-   * @param endTime 取得期間の終了時間
-   * @return HttpResponse
-   */
-  @GetMapping
-  @RequestMapping(value = "/cpu-usagewithexception", method = RequestMethod.GET)
-  public ResponseEntity<?> cpuUsageWithException( @RequestParam("starttime")String startTime,@RequestParam("endtime")String endTime) {
-    try {
-
-      // TODO APIリクエスト時の処理 リクエスト項目をJSON化して出力する？
-      systemLogger.info("cpuUsageWithExceptionAPI start: " + startTime + endTime);
-
-      SimpleDateFormat dtFt = new SimpleDateFormat("yyyyMMddHHmmss");
-      Date startTimeDate = dtFt.parse(startTime);
-      Date endTimeDate = dtFt.parse(endTime);
-      CpuUsageApiRequest request = new CpuUsageApiRequest(startTimeDate, endTimeDate);
-      CpuUsageApiResponse response = cpuUsageService.getCpuUsage(request);
-
-      // ダミーのレスポンスを作成する --START
-      SimpleDateFormat dummyDtFt = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-      CpuUsageData[] cpuUsageDatas = new CpuUsageData[]{
-          new CpuUsageData(dummyDtFt.parse("2023/05/07 18:00:00"), 20.98),
-          new CpuUsageData(dummyDtFt.parse("2023/05/07 18:10:00"), 30.98),
-          new CpuUsageData(dummyDtFt.parse("2023/05/07 18:20:00"), 40.98),
-          new CpuUsageData(dummyDtFt.parse("2023/05/07 18:30:00"), 20.88),
-          new CpuUsageData(dummyDtFt.parse("2023/05/07 18:40:00"), 20.68),
-          new CpuUsageData(dummyDtFt.parse("2023/05/07 18:50:00"), 25.98),
-          new CpuUsageData(dummyDtFt.parse("2023/05/07 19:00:00"), 20.98),
-          new CpuUsageData(dummyDtFt.parse("2023/05/07 19:10:00"), 20.98),
-          new CpuUsageData(dummyDtFt.parse("2023/05/07 19:20:00"), 30.98),
-          new CpuUsageData(dummyDtFt.parse("2023/05/07 19:30:00"), 20.98),
-          new CpuUsageData(dummyDtFt.parse("2023/05/07 19:40:00"), 70.68),
-          new CpuUsageData(dummyDtFt.parse("2023/05/07 19:50:00"), 10.28),
-          new CpuUsageData(dummyDtFt.parse("2023/05/07 20:00:00"), 20.98)
-      };
-      response = new CpuUsageApiResponse(startTimeDate, endTimeDate, cpuUsageDatas);
-      // ダミーのレスポンスを作成する --END
-
-      // TODO APIリクエスト時の処理 リクエスト項目をJSON化して出力する？
-      systemLogger.info("cpuUsageWithExceptionAPI end: " + startTime + endTime);
-      // TODO オペレーションログの出力内容決定
-      operationLogger.info("cpuUsageWithExceptionAPI operationTime:" + "1.2345 sec");
-
-      return ResponseEntity.ok(response);
-    } catch (ParseException pe) {
-      // TODO リクエストの単体チェック（Filterなどの設定する？）
-      errorLogger.error("cpuUsageWithException:ParseException",pe);
-      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    } catch (ApplicationException ae){
-      errorLogger.error("cpuUsageWithException:ApplicationException",ae);
-      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-    } catch (Exception e) {
-      errorLogger.error("cpuUsageWithException:Exception",e);
-      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-  }
 
   /**
    * postgresのプロセス情報を取得するAPI
@@ -174,27 +108,35 @@ public class VisualizeController {
   @RequestMapping(value = "/processes", method = RequestMethod.GET)
   public ResponseEntity<?> processes(@RequestParam("starttime")String startTime,@RequestParam("endtime")String endTime) {
     try {
+      long methodBeginTime = System.currentTimeMillis();
+      systemLogger.info("processes start。 startTime:" + startTime + " endTime:" + endTime);
+
       SimpleDateFormat dtFt = new SimpleDateFormat(STRING_TO_DATE);
       Date startTimeDate = dtFt.parse(startTime);
       Date endTimeDate = dtFt.parse(endTime);
       PostgresProcessApiRequest request = new PostgresProcessApiRequest(startTimeDate, endTimeDate);
       PostgresProcessApiResponse response = postgresProcessService.getPostgresProcess(request);
 
-      // ダミーのレスポンスを作成する --START
-      // サービスの実装が完了次第、作成する予定
-      response = new PostgresProcessApiResponse(startTimeDate, endTimeDate, true,false,
-       true,false,true,true,false,true);
-      // ダミーのレスポンスを作成する --END
+      systemLogger.info("processes end。 Response:" + response);
+      long methodEndTime = System.currentTimeMillis();
+      operationLogger.info("processes operationTime:" + (methodEndTime - methodBeginTime) + " ms");
 
       return ResponseEntity.ok(response);
     }
-    catch (ParseException e) {
+    catch (ParseException pe) {
+      errorLogger.error("processes:ParseException", pe);
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
+    catch (ApplicationException ae) {
+      errorLogger.error("processes:ApplicationException", ae);
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
     catch(Exception e) {
+      errorLogger.error("processes:Exception", e);
       return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
+
 
   /**
    * クエリの平均実行時間を取得するAPI
@@ -210,6 +152,9 @@ public class VisualizeController {
       @RequestParam("endtime")String endTime,
       @RequestParam("kind")Short kind) {
     try {
+      long methodBeginTime = System.currentTimeMillis();
+      systemLogger.info("averageQueryTime start。 startTime:" + startTime + " endTime:" + endTime);
+
       if(kind != 1 && kind != 2 && kind != 3 && kind != 4) {
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
       }
@@ -219,17 +164,22 @@ public class VisualizeController {
       AverageQueryTimeApiRequest request = new AverageQueryTimeApiRequest(startTimeDate, endTimeDate, kind);
       AverageQueryTimeApiResponse response = averageQueryTimeService.getAverageQueryTime(request);
 
-      // ダミーのレスポンスを作成する --START
-      // サービスの実装が完了次第、作成する予定
-      response = new AverageQueryTimeApiResponse(startTimeDate, endTimeDate, kind, 1.33);
-      // ダミーのレスポンスを作成する --END
+      systemLogger.info("averageQueryTime end。 Response:" + response);
+      long methodEndTime = System.currentTimeMillis();
+      operationLogger.info("averageQueryTime operationTime:" + (methodEndTime - methodBeginTime) + " ms");
 
       return ResponseEntity.ok(response);
     }
-    catch (ParseException e) {
+    catch (ParseException pe) {
+      errorLogger.error("averageQueryTime:ParseException", pe);
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
+    catch (ApplicationException ae) {
+      errorLogger.error("averageQueryTime:ApplicationException", ae);
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
     catch(Exception e) {
+      errorLogger.error("averageQueryTime:Exception", e);
       return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
@@ -248,23 +198,31 @@ public class VisualizeController {
       @RequestParam("endtime")String endTime,
       @RequestParam("dbname")String dbname) {
     try {
+      long methodBeginTime = System.currentTimeMillis();
+      systemLogger.info("hitRate start。 startTime:" + startTime + " endTime:" + endTime);
+
       SimpleDateFormat dtFt = new SimpleDateFormat(STRING_TO_DATE);
       Date startTimeDate = dtFt.parse(startTime);
       Date endTimeDate = dtFt.parse(endTime);
       CacheHitRateApiRequest request = new CacheHitRateApiRequest(startTimeDate, endTimeDate, dbname);
       CacheHitRateApiResponse response = cacheHitRateService.getCacheHitRate(request);
 
-      // ダミーのレスポンスを作成する --START
-      // サービスの実装が完了次第、作成する予定
-      response = new CacheHitRateApiResponse(startTimeDate, endTimeDate, "databasename", 0.25);
-      // ダミーのレスポンスを作成する --END
+      systemLogger.info("hitRate end。 Response:" + response);
+      long methodEndTime = System.currentTimeMillis();
+      operationLogger.info("hitRate operationTime:" + (methodEndTime - methodBeginTime) + " ms");
 
       return ResponseEntity.ok(response);
     }
-    catch (ParseException e) {
+    catch (ParseException pe) {
+      errorLogger.error("hitRate:ParseException", pe);
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
+    catch (ApplicationException ae) {
+      errorLogger.error("hitRate:ApplicationException", ae);
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
     catch(Exception e) {
+      errorLogger.error("hitRate:Exception", e);
       return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
@@ -283,23 +241,31 @@ public class VisualizeController {
       @RequestParam("endtime")String endTime,
       @RequestParam("querytime")Double queryTime) {
     try {
+      long methodBeginTime = System.currentTimeMillis();
+      systemLogger.info("slowQueryCounts start。 startTime:" + startTime + " endTime:" + endTime);
+
       SimpleDateFormat dtFt = new SimpleDateFormat(STRING_TO_DATE);
       Date startTimeDate = dtFt.parse(startTime);
       Date endTimeDate = dtFt.parse(endTime);
       SlowQueryCountApiRequest request = new SlowQueryCountApiRequest(startTimeDate, endTimeDate, queryTime);
       SlowQueryCountApiResponse response = slowQueryCountService.getSlowQueryCount(request);
 
-      // ダミーのレスポンスを作成する --START
-      // サービスの実装が完了次第、作成する予定
-      response = new SlowQueryCountApiResponse(startTimeDate, endTimeDate, 1.00, 3);
-      // ダミーのレスポンスを作成する --END
+      systemLogger.info("slowQueryCounts end。 Response:" + response);
+      long methodEndTime = System.currentTimeMillis();
+      operationLogger.info("slowQueryCounts operationTime:" + (methodEndTime - methodBeginTime) + " ms");
 
       return ResponseEntity.ok(response);
     }
-    catch (ParseException e) {
+    catch (ParseException pe) {
+      errorLogger.error("slowQueryCounts:ParseException", pe);
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
+    catch (ApplicationException ae) {
+      errorLogger.error("slowQueryCounts:ApplicationException", ae);
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
     catch(Exception e) {
+      errorLogger.error("slowQueryCounts:Exception", e);
       return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
