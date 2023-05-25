fb6d0802a5d0f039e30e51b3910aeb43d0e67754
r-horikiri <r-horikiri@medi-system.co.jp>
2023-05-25 17:55:18 +0900

    feat: SshUtil

--- src/main/java/jp/ac/databaseexplorer/async/job/visualization/SarCpuJob.java
+++ src/main/java/jp/ac/databaseexplorer/async/job/visualization/SarCpuJob.java
@@ -1,6 +1,8 @@
 package jp.ac.databaseexplorer.async.job.visualization;
 
+import jp.ac.databaseexplorer.common.component.os.SshUtil;
 import jp.ac.databaseexplorer.common.exception.ApplicationException;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Service;
  * sarコマンドでCPUのシステム統計情報を収集します。
  */
 @Service
+@RequiredArgsConstructor
 public class SarCpuJob extends VisualizeJobBase {
 
   /**
@@ -17,10 +20,16 @@ public class SarCpuJob extends VisualizeJobBase {
   private static final Logger systemLogger =  LoggerFactory.getLogger("SYSTEM_LOG");
 
   /**
+   * Bean
+   */
+  private final SshUtil ssh;
+
+  /**
    * CPUのシステム統計情報を収集します。
    */
   @Override
   public void execute() throws ApplicationException {
     systemLogger.info("SarCpuJob#execute() called.");
+    System.out.println(ssh.execute("ls"));
   }
 }
