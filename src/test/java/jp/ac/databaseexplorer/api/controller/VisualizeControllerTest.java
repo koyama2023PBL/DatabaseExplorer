e16f01f110f3d7ebf8f9b78abe57149869161a72
Kyoko Yoshida <a2244ky@aiit.ac.jp>
2023-06-02 00:44:24 +0900

    test: cpuUsageメソッドが正しいレスポンスを返すか（正常系1）mockを使わない状態（が，まだJsonを比較している）（動作確認済）

--- src/test/java/jp/ac/databaseexplorer/api/controller/VisualizeControllerTest.java
+++ src/test/java/jp/ac/databaseexplorer/api/controller/VisualizeControllerTest.java
@@ -1,63 +1,70 @@
 package jp.ac.databaseexplorer.api.controller;
-
-import jp.ac.databaseexplorer.api.model.visualization.CpuUsageApiRequest;
-import jp.ac.databaseexplorer.api.model.visualization.CpuUsageApiResponse;
-import jp.ac.databaseexplorer.api.model.visualization.CpuUsageData;
-import jp.ac.databaseexplorer.api.service.visualization.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import java.nio.charset.StandardCharsets;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
 class VisualizeControllerTest {
 
-  private final CpuUsageService cpuUsageService = Mockito.mock(CpuUsageService.class);
-  private final PostgresProcessService postgresProcessService = Mockito.mock(PostgresProcessService.class);
-  private final AverageQueryTimeService averageQueryTimeService = Mockito.mock(AverageQueryTimeService.class);
-  private final CacheHitRateService cacheHitRateService = Mockito.mock(CacheHitRateService.class);
-  private final SlowQueryCountService slowQueryCountService = Mockito.mock(SlowQueryCountService.class);
+  private MockMvc mockMvc;
 
-  private final VisualizeController visualizeController = new VisualizeController(cpuUsageService, postgresProcessService, averageQueryTimeService, cacheHitRateService, slowQueryCountService);
+  @Autowired
+  VisualizeController visualizeController;
+
+  @BeforeEach
+  public void setup() {
+    this.mockMvc = MockMvcBuilders.standaloneSetup(visualizeController).build();
+  }
 
   //レスポンス200かつ正常にデータが取得されているパターン
   @Test
   public void testCpuUsageReturnsCorrectResponse() throws Exception {
-    // API側から呼ばれた場合の挙動を定義
-    String startTimeStr = "20230501000120";
-    String endTimeStr = "20230501000130";
-    SimpleDateFormat dtFt = new SimpleDateFormat("yyyyMMddHHmmss");
-    Date startTimeDate = dtFt.parse(startTimeStr);
-    Date endTimeDate = dtFt.parse(endTimeStr);
-    CpuUsageApiRequest request = new CpuUsageApiRequest(startTimeDate, endTimeDate);
-    // 期待するレスポンスを定義
-    SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-    Date date1 = formatter.parse("2023/05/01 00:01:20");
-    Date date2 = formatter.parse("2023/05/01 00:01:30");
-    double maxUsage = 100;
-    double actualIdleRate1 = 41.09;
-    double actualIdleRate2 = 31.63;
-    CpuUsageData data1 = new CpuUsageData(date1, maxUsage-actualIdleRate1);
-    CpuUsageData data2 = new CpuUsageData(date2, maxUsage-actualIdleRate2);
-    CpuUsageApiResponse expectedResponse = new CpuUsageApiResponse(date1, date2, new CpuUsageData[]{data1, data2});
-    when(cpuUsageService.getCpuUsage(request)).thenReturn(expectedResponse);
 
-    // Act
-    ResponseEntity<?> responseEntity = visualizeController.cpuUsage(startTimeStr, endTimeStr);
+    // Arrange
+    String startTimeStr = "20230501001200";
+    String endTimeStr = "20230501001210";
+    Double maxUsage = 100D;
+    Double idle1 = 52.12;
+    Double idle2 = 29.96;
+    String expectResponse = "{\"starttime\":\"2023/05/01 00:12:00\",\"endtime\":\"2023/05/01 00:12:10\"," +
+        "\"data\":[{\"date\":\"2023/05/01 00:12:00\",\"usage\":" + (maxUsage - idle1) + "}," +
+        "{\"date\":\"2023/05/01 00:12:10\",\"usage\":" + (maxUsage - idle2) + "}]}";
 
-    // Assert
-    assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-    assertEquals(expectedResponse, responseEntity.getBody());
+    //when
+    //Json確認するver
+    String result = mockMvc.perform(get("/database-explorer/api/visualization/cpu-usage")
+      .param("starttime", startTimeStr)
+      .param("endtime", endTimeStr))
+      .andExpect(status().isOk())
+      .andExpect(content().json(expectResponse))
+      .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+        //.andReturn();
+
+    // Parse response body
+    // これはデシリアライズができないからダメ
+//    String responseBody = mvcResult.getResponse().getContentAsString();
+//    ObjectMapper objectMapper = new ObjectMapper();
+//    CpuUsageApiResponse actualResponse = objectMapper.readValue(responseBody, CpuUsageApiResponse.class);
+
+    //then
+    JSONAssert.assertEquals(expectResponse, result, JSONCompareMode.STRICT);
+    //assertEquals(expectResponse, result);
+
   }
 
-  //レスポンス200かつ正常にデータが取得されているパターン
+
 
 
 //  @Test
