38fc337c36b131499a4a4299ec59771ceec812c9
Kyoko Yoshida <a2244ky@aiit.ac.jp>
2023-06-04 15:46:58 +0900

    fix: 同じ処理の重複記述を削除

--- src/test/java/jp/ac/databaseexplorer/api/controller/VisualizeControllerTest.java
+++ src/test/java/jp/ac/databaseexplorer/api/controller/VisualizeControllerTest.java
@@ -1,20 +1,13 @@
 package jp.ac.databaseexplorer.api.controller;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.skyscreamer.jsonassert.JSONAssert;
-import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.nio.charset.StandardCharsets;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 //@RunWith(SpringJUnit4ClassRunner.class)
@@ -44,26 +37,13 @@ class VisualizeControllerTest {
         "\"data\":[{\"date\":\"2023/05/01 00:12:00\",\"usage\":" + (maxUsage - idle1) + "}," +
         "{\"date\":\"2023/05/01 00:12:10\",\"usage\":" + (maxUsage - idle2) + "}]}";
 
-    //when
-    //Json確認するver
-    String result = mockMvc.perform(get("/database-explorer/api/visualization/cpu-usage")
+    // Act
+    mockMvc.perform(get("/database-explorer/api/visualization/cpu-usage")
       .param("starttime", startTimeStr)
       .param("endtime", endTimeStr))
       .andExpect(status().isOk())
-      .andDo(MockMvcResultHandlers.print())
       .andExpect(content().json(expectResponse))
-      .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
-        //.andReturn();
-
-    // Parse response body
-    // これはデシリアライズができないからダメ
-//    String responseBody = mvcResult.getResponse().getContentAsString();
-//    ObjectMapper objectMapper = new ObjectMapper();
-//    CpuUsageApiResponse actualResponse = objectMapper.readValue(responseBody, CpuUsageApiResponse.class);
-
-    //then
-    JSONAssert.assertEquals(expectResponse, result, JSONCompareMode.STRICT);
-    //assertEquals(expectResponse, result);
+      .andReturn();
 
   }
 
