5ad9985eb368bdbb731427ca260fa7f9d9f499f6
Kyoko Yoshida <a2244ky@aiit.ac.jp>
2023-06-04 21:41:18 +0900

    test: CPU使用率のテストケースを追加

--- src/test/java/jp/ac/databaseexplorer/api/controller/VisualizeControllerTest.java
+++ src/test/java/jp/ac/databaseexplorer/api/controller/VisualizeControllerTest.java
@@ -25,7 +25,7 @@ class VisualizeControllerTest {
 
   //レスポンス200かつ正常にデータが取得されているパターン
   @Test
-  public void testCpuUsageReturnsCorrectResponse() throws Exception {
+  public void testCpuUsageDataIsCorrectlyRetrieved() throws Exception {
 
     // Arrange
     String startTimeStr = "20230501001200";
@@ -46,6 +46,132 @@ class VisualizeControllerTest {
       .andReturn();
 
   }
+
+  //日付の指定が不正なパターン
+  //1: 取得していない日付が含まれる
+  //2: その間にデータがない
+  @Test
+  public void testCpuUsageEndTimeIsOutOfRange() throws Exception {
+
+    // Arrange
+    String startTimeStr = "20230528235950";
+    String endTimeStr = "20240501001210";
+    Double maxUsage = 100D;
+    Double idle1 = 41.69;
+    //Double idle2 = 29.96;
+    String expectResponse = "{\"starttime\":\"2023/05/28 23:59:50\",\"endtime\":\"2024/05/01 00:12:10\"," +
+        "\"data\":[{\"date\":\"2023/05/28 23:59:50\",\"usage\":" + (maxUsage - idle1) + "}]}";
+
+    // Act
+    mockMvc.perform(get("/database-explorer/api/visualization/cpu-usage")
+            .param("starttime", startTimeStr)
+            .param("endtime", endTimeStr))
+        .andExpect(status().isOk())
+        .andExpect(content().json(expectResponse))
+        .andReturn();
+
+  }
+
+  @Test
+  public void testCpuUsageStartTimeIsOutOfRange() throws Exception {
+
+    // Arrange
+    String startTimeStr = "19900528235950";
+    String endTimeStr = "20230501000010";
+    Double maxUsage = 100D;
+    Double idle1 = 51.23;
+    Double idle2 = 50.56;
+    String expectResponse = "{\"starttime\":\"1990/05/28 23:59:50\",\"endtime\":\"2023/05/01 00:00:10\"," +
+        "\"data\":[{\"date\":\"2023/05/01 00:00:00\",\"usage\":" + (maxUsage - idle1) + "}," +
+        "{\"date\":\"2023/05/01 00:00:10\",\"usage\":" + (maxUsage - idle2) + "}]}";
+
+    // Act
+    mockMvc.perform(get("/database-explorer/api/visualization/cpu-usage")
+            .param("starttime", startTimeStr)
+            .param("endtime", endTimeStr))
+        .andExpect(status().isOk())
+        .andExpect(content().json(expectResponse))
+        .andReturn();
+
+  }
+
+  @Test
+  public void testCpuUsageBothTimesAreBeforeRange() throws Exception {
+
+    // Arrange
+    String startTimeStr = "19900501235950";
+    String endTimeStr = "19900501000010";
+//    Double maxUsage = 100D;
+//    Double idle1 = 51.23;
+//    Double idle2 = 50.56;
+//    String expectResponse = "{\"starttime\":\"1990/05/01 23:59:50\",\"endtime\":\"1990/05/01 00:00:10\"," +
+//        "\"data\":[]}";
+
+    // Act
+    mockMvc.perform(get("/database-explorer/api/visualization/cpu-usage")
+            .param("starttime", startTimeStr)
+            .param("endtime", endTimeStr))
+        .andExpect(status().is5xxServerError())
+        .andReturn();
+
+  }
+
+  @Test
+  public void testCpuUsageBothTimesAreAfterRange() throws Exception {
+
+    // Arrange
+    String startTimeStr = "20250501235950";
+    String endTimeStr = "20250501000010";
+//    Double maxUsage = 100D;
+//    Double idle1 = 51.23;
+//    Double idle2 = 50.56;
+//    String expectResponse = "{\"starttime\":\"2025/05/01 23:59:50\",\"endtime\":\"2025/05/01 00:00:10\"," +
+//        "\"data\":[]}";
+
+    // Act
+    mockMvc.perform(get("/database-explorer/api/visualization/cpu-usage")
+            .param("starttime", startTimeStr)
+            .param("endtime", endTimeStr))
+        .andExpect(status().is5xxServerError())
+        .andReturn();
+
+  }
+
+  @Test
+  public void testCpuUsageTimesAreInverted() throws Exception {
+
+    // Arrange
+    String startTimeStr = "20230501001210";
+    String endTimeStr = "20230501001200";
+//    Double maxUsage = 100D;
+//    Double idle1 = 52.12;
+//    Double idle2 = 29.96;
+//    String expectResponse = "{\"starttime\":\"2023/05/01 00:12:10\",\"endtime\":\"2023/05/01 00:12:00\"," +
+//        "\"data\":[]}";
+
+    // Act
+    mockMvc.perform(get("/database-explorer/api/visualization/cpu-usage")
+            .param("starttime", startTimeStr)
+            .param("endtime", endTimeStr))
+        .andExpect(status().is5xxServerError())
+        .andReturn();
+
+  }
+
+  @Test
+  public void testCpuUsageTimeIsInvalid() throws Exception {
+
+    // Arrange
+    String startTimeStr = "";
+    String endTimeStr = "20230501001200";
+
+    // Act
+    mockMvc.perform(get("/database-explorer/api/visualization/cpu-usage")
+            .param("starttime", startTimeStr)
+            .param("endtime", endTimeStr))
+        .andExpect(status().isBadRequest());
+  }
+
 
 
 
