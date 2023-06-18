99c43595947b1c0600cdca135d9ed5a88a6f722d
Feng Yiwei <a2235yf@aiit.ac.jp>
2023-06-18 20:51:44 +0900

    feat:クエリ実行時間の取得、および当該情報をCSVファイルに書込み

--- src/main/java/jp/ac/databaseexplorer/storage/visualization/QueryTime.java
+++ src/main/java/jp/ac/databaseexplorer/storage/visualization/QueryTime.java
@@ -1,6 +1,10 @@
 package jp.ac.databaseexplorer.storage.visualization;
 
 import com.opencsv.bean.CsvBindByPosition;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jp.ac.databaseexplorer.storage.base.CsvWithTimestampModelBase;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -8,13 +12,18 @@ import lombok.EqualsAndHashCode;
 /**
  * DBより取得したクエリの実行時間（プロセスごと）を保持するモデルクラス
  */
+@Entity
 @Data
 @EqualsAndHashCode(callSuper = true)
+@Table(name = "pg_stat_activity")
 public class QueryTime extends CsvWithTimestampModelBase {
   @CsvBindByPosition(position = 1)
+  @Column(name = "pid")
+  @Id
   private Integer pid;
 
   @CsvBindByPosition(position = 2)
+  @Column(name = "execution_time")
   private Double queryTime;
 
 }
