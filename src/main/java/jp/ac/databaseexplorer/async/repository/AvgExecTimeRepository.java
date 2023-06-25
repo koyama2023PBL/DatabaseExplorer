ee4503086831a0c8b0bc13189a3aaa55cdf425bd
Kyoko Yoshida <a2244ky@aiit.ac.jp>
2023-06-25 15:10:03 +0900

    feat: クエリ実行時間取得の非同期処理を実装

---
+++ src/main/java/jp/ac/databaseexplorer/async/repository/AvgExecTimeRepository.java
@@ -0,0 +1,28 @@
+package jp.ac.databaseexplorer.async.repository;
+
+import jp.ac.databaseexplorer.storage.visualization.AvgExecTime;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AvgExecTimeRepository extends JpaRepository<AvgExecTime, Short> {
+  @Query(value = "SELECT " +
+      "CASE " +
+      "WHEN query ILIKE 'SELECT%' THEN 1 " +
+      "WHEN query ILIKE 'INSERT%' THEN 2 " +
+      "WHEN query ILIKE 'UPDATE%' THEN 3 " +
+      "WHEN query ILIKE 'DELETE%' THEN 4 " +
+      "END AS kind, " +
+      "SUM(calls) AS calls, " +
+      "SUM(total_exec_time) AS total_exec_time " +
+      "FROM " +
+      "pg_stat_statements " +
+      "WHERE " +
+      "query NOT ILIKE '%pg_stat_statements%' " +
+      "GROUP BY " +
+      "kind ;", nativeQuery = true)
+  List<AvgExecTime> getAvgExecTime();
+}
--- src/main/java/jp/ac/databaseexplorer/storage/visualization/AvgExecTime.java
+++ src/main/java/jp/ac/databaseexplorer/storage/visualization/AvgExecTime.java
@@ -1,6 +1,9 @@
 package jp.ac.databaseexplorer.storage.visualization;
 
 import com.opencsv.bean.CsvBindByPosition;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jp.ac.databaseexplorer.storage.base.CsvWithTimestampModelBase;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -9,16 +12,22 @@ import lombok.EqualsAndHashCode;
  * データベースから取得したクエリ実行時間の平均を格納するモデルクラス
  */
 @Data
+@Entity
 @EqualsAndHashCode(callSuper = true)
 public class AvgExecTime extends CsvWithTimestampModelBase {
 
   @CsvBindByPosition(position = 1)
+  @Column(name = "kind")
+  @Id
   private Short kind;
 
   @CsvBindByPosition(position = 2)
+  @Column(name = "calls")
   private Long calls;
 
   @CsvBindByPosition(position = 3)
+  @Column(name = "total_exec_time")
   private Double totalExecTime;
+
 
 }
